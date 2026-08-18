285efb7b829bf972d9c7daf2706aab84759f346e
Krimon Gurung <krimongurung524@gmail.com>
2026-08-18 14:52:31 +0545

    model class is added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Room(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
