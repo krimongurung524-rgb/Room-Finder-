6d2748bdffbe0e596529d81e780b7c2c308941c5
Krimon Gurung <krimongurung524@gmail.com>
2026-08-27 12:23:37 +0545

    rooms search is added on url.py

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.room_list, name='room_list'),
     path('create/', views.room_create, name='room_create'),
     path('my-page/', views.my_static_page, name='my_static_page'),
+    path('search/', views.room_search, name='room_search'),
     path('<slug:slug>/', views.room_detail, name='room_detail'),
 ]
